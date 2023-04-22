0b743631a4050408bb7705a09892f1cd11252027
Vitoserra <45820315+Vitoserra@users.noreply.github.com>
2023-04-22 11:44:30 +0200

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -19,11 +19,6 @@ Respond using Markdown.`;
 
 export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
-  'gpt-4',
-  'gpt-4-32k',
-  // 'gpt-3.5-turbo-0301',
-  // 'gpt-4-0314',
-  // 'gpt-4-32k-0314',
 ];
 
 export const defaultModel = 'gpt-3.5-turbo';
